a57dbe7629949af170e67a6caaf07aecab44c5f1
prmstrgt <pramestiregitaa@gmail.com>
2020-10-28 23:31:48 +0700

    slicing ForgotPassword

---
+++ src/screens/ForgotPassword/index.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+import {
+  Body,
+  Button,
+  Card,
+  CardItem,
+  Form,
+  H1,
+  Input,
+  Item,
+  Label,
+} from 'native-base';
+
+import style from './style';
+
+export default class ForgotPassword extends Component {
+  render() {
+    return (
+      <View style={style.parent}>
+        <View style={style.title}>
+          <H1 style={style.titleText}>Forgot Password</H1>
+        </View>
+        <View style={style.info}>
+          <Text style={style.infoText}>
+            Please, enter your email address. You will receive a link to create
+            a new password via email.
+          </Text>
+        </View>
+        <Form>
+          <Card style={style.inputCard}>
+            <CardItem>
+              <Body>
+                <Item style={style.inputWrapper} floatingLabel>
+                  <Label style={style.label}>Email</Label>
+                  <Input style={style.input} />
+                </Item>
+              </Body>
+            </CardItem>
+          </Card>
+          <View style={style.btnWrapper}>
+            <Button style={style.btn}>
+              <Text style={style.btnText}>send</Text>
+            </Button>
+          </View>
+        </Form>
+      </View>
+    );
+  }
+}
---
+++ src/screens/ForgotPassword/style.js
@@ -0,0 +1,56 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  parent: {
+    flex: 1,
+    marginHorizontal: 25,
+  },
+  title: {
+    marginTop: 25,
+    marginBottom: 60,
+  },
+  titleText: {
+    fontWeight: 'bold',
+  },
+  info: {
+    marginBottom: 15,
+  },
+  infoText: {
+    fontSize: 15,
+  },
+  inputCard: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+  },
+  inputWrapper: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  label: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 16,
+  },
+  input: {
+    color: 'black',
+    fontSize: 19,
+  },
+  btnWrapper: {
+    flex: 1,
+    marginTop: 70,
+  },
+  btn: {
+    width: '100%',
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#884A6F',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnText: {
+    fontSize: 16,
+    textTransform: 'uppercase',
+    color: 'white',
+  },
+});
