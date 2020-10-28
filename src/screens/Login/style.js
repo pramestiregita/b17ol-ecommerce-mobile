2d470c8c98cd60737a8c85007e2a28982cf29fce
prmstrgt <pramestiregitaa@gmail.com>
2020-10-28 23:15:29 +0700

    slicing Login

---
+++ src/screens/Login/style.js
@@ -0,0 +1,63 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  parent: {
+    flex: 1,
+    marginHorizontal: 25,
+  },
+  title: {
+    marginTop: 25,
+    marginBottom: 60,
+  },
+  titleText: {
+    fontWeight: 'bold',
+  },
+  inputCard: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+  },
+  inputWrapper: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  label: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 16,
+  },
+  input: {
+    color: 'black',
+    fontSize: 19,
+  },
+  linkWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    marginVertical: 8,
+  },
+  textLink: {
+    fontSize: 16,
+  },
+  iconLink: {
+    color: '#884A6F',
+    marginLeft: 5,
+  },
+  btnWrapper: {
+    flex: 1,
+    marginTop: 15,
+  },
+  btn: {
+    width: '100%',
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#884A6F',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnText: {
+    fontSize: 16,
+    textTransform: 'uppercase',
+    color: 'white',
+  },
+});
