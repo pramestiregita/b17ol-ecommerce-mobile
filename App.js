b89427e70f46eeb3a7a3cbc0640316ceec48ada7
prmstrgt <pramestiregitaa@gmail.com>
2020-11-05 06:39:36 +0700

    react-persist implementation

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 import Main from './src/screens/Main';
 import store from './src/redux/store';
@@ -7,8 +8,10 @@ import store from './src/redux/store';
 export default class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Main />
+      <Provider store={store().store}>
+        <PersistGate loading={null} persistor={store().persistor}>
+          <Main />
+        </PersistGate>
       </Provider>
     );
   }
