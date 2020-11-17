595b3e3e3ab54dbb3bc414521686a3491c1163c0
prmstrgt <pramestiregitaa@gmail.com>
2020-11-17 08:43:45 +0700

    form validation for Login

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import {Alert, Text, View} from 'react-native';
+import {Alert, Text, View, TouchableOpacity, ScrollView} from 'react-native';
 import {
   Body,
   Button,
@@ -15,10 +15,18 @@ import {
   Label,
 } from 'native-base';
 import {connect} from 'react-redux';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 
 import style from './style';
 import authAction from '../../redux/actions/auth';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+
+const loginSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Please input a valid email')
+    .required('Please input your email'),
+  password: Yup.string().required('Please input your password'),
+});
 
 class Login extends Component {
   state = {
@@ -27,9 +35,9 @@ class Login extends Component {
     alertMsg: '',
   };
 
-  login = () => {
-    const {email, password} = this.state;
-    const data = {email, password};
+  login = (data) => {
+    // const {email, password} = this.state;
+    // const data = {email, password};
     this.props.doLogin(data);
   };
 
@@ -47,59 +55,95 @@ class Login extends Component {
 
   render() {
     return (
-      <Container>
-        <Content>
-          <View style={style.parent}>
-            <View style={style.title}>
-              <H1 style={style.titleText}>Login</H1>
-            </View>
-            <Form>
-              <Card style={style.inputCard}>
-                <CardItem>
-                  <Body>
-                    <Item style={style.inputWrapper} floatingLabel>
-                      <Label style={style.label}>Email</Label>
-                      <Input
-                        onChangeText={(email) => this.setState({email})}
-                        style={style.input}
-                      />
-                    </Item>
-                  </Body>
-                </CardItem>
-              </Card>
-              <Card style={style.inputCard}>
-                <CardItem>
-                  <Body>
-                    <Item style={style.inputWrapper} floatingLabel>
-                      <Label style={style.label}>Password</Label>
-                      <Input
-                        onChangeText={(password) => this.setState({password})}
-                        style={style.input}
-                        secureTextEntry
-                      />
-                    </Item>
-                  </Body>
-                </CardItem>
-              </Card>
-              <TouchableOpacity
-                onPress={() => this.props.navigation.navigate('ForgotPassword')}
-                style={style.linkWrapper}>
-                <Text style={style.textLink}>Forgot your password?</Text>
-                <Icon
-                  style={style.iconLink}
-                  name="long-arrow-right"
-                  size={19}
-                />
-              </TouchableOpacity>
-              <View style={style.btnWrapper}>
-                <Button onPress={this.login} style={style.btn}>
-                  <Text style={style.btnText}>login</Text>
-                </Button>
-              </View>
-            </Form>
+      <View style={style.parent}>
+        <ScrollView>
+          <View style={style.title}>
+            <H1 style={style.titleText}>Login</H1>
           </View>
-        </Content>
-      </Container>
+          <Formik
+            initialValues={{email: '', password: ''}}
+            validationSchema={loginSchema}
+            onSubmit={(values) => this.login(values)}>
+            {({
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              values,
+              errors,
+              touched,
+            }) => (
+              <View>
+                <Card style={style.inputCard}>
+                  <CardItem>
+                    <Body>
+                      <Item style={style.inputWrapper} floatingLabel>
+                        <Label style={style.label}>Email</Label>
+                        <Input
+                          onChangeText={handleChange('email')}
+                          onBlur={handleBlur('email')}
+                          style={style.input}
+                          value={values.email}
+                        />
+                      </Item>
+                      {errors.email && touched.email ? (
+                        <View style={style.alertWrapper}>
+                          <Icon
+                            style={style.alertIcon}
+                            name="exclamation-triangle"
+                          />
+                          <Text style={style.alert}>{errors.email}</Text>
+                        </View>
+                      ) : null}
+                    </Body>
+                  </CardItem>
+                </Card>
+                <Card style={style.inputCard}>
+                  <CardItem>
+                    <Body>
+                      <Item style={style.inputWrapper} floatingLabel>
+                        <Label style={style.label}>Password</Label>
+                        <Input
+                          onChangeText={handleChange('password')}
+                          onBlur={handleBlur('password')}
+                          style={style.input}
+                          secureTextEntry
+                          value={values.password}
+                        />
+                      </Item>
+                      {errors.password && touched.password ? (
+                        <View style={style.alertWrapper}>
+                          <Icon
+                            style={style.alertIcon}
+                            name="exclamation-triangle"
+                          />
+                          <Text style={style.alert}>{errors.password}</Text>
+                        </View>
+                      ) : null}
+                    </Body>
+                  </CardItem>
+                </Card>
+                <TouchableOpacity
+                  onPress={() =>
+                    this.props.navigation.navigate('ForgotPassword')
+                  }
+                  style={style.linkWrapper}>
+                  <Text style={style.textLink}>Forgot your password?</Text>
+                  <Icon
+                    style={style.iconLink}
+                    name="long-arrow-right"
+                    size={19}
+                  />
+                </TouchableOpacity>
+                <View style={style.btnWrapper}>
+                  <Button onPress={handleSubmit} style={style.btn}>
+                    <Text style={style.btnText}>login</Text>
+                  </Button>
+                </View>
+              </View>
+            )}
+          </Formik>
+        </ScrollView>
+      </View>
     );
   }
 }
